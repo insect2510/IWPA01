6f8d3896a3c2cddbf004a21b6633a0c30ce0f6a4
insect2510 <155474669+insect2510@users.noreply.github.com>
2024-02-10 17:41:36 +0100

    Global Nav als CSS Farben

--- carbon.js
+++ carbon.js
@@ -277,13 +277,13 @@ function setLanguage(language) {
   let langId;
   langId = document.getElementById("langselect").getElementsByTagName("button")
   if (language == 0) {
-    document.getElementById("hauptnavigation").style = "float: left;";
+    document.getElementById("hauptnavigation").style.float = "left";
     langId[0].classList.add("active");
     langId[1].classList.remove("active");
 
   }
   else {
-    document.getElementById("hauptnavigation").style = "float: right;";
+    document.getElementById("hauptnavigation").style.float = "right";
     langId[1].classList.add("active");
     langId[0].classList.remove("active");
 
