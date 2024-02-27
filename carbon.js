ca18a0035751af2949d5f835d7414e0c118b0b1e
insect2510 <155474669+insect2510@users.noreply.github.com>
2024-02-27 01:23:43 +0100

    Filter Bug beseitigt. Rename langButtonId

--- carbon.js
+++ carbon.js
@@ -1,10 +1,11 @@
 // Globale Variablen definieren
 let sortIcon = "bi bi-caret-up-fill";
 let tableHead = ["Company", "Country", "Emission", "Ratio in %"], searchFor = tableHead[0];
-let htmlFilter, htmlTabletotalEmission, myArr, tableLenght;
+let htmlFilter, htmlTabletotalEmission, myArr, tableLenght, filterButtonId;
 let iconStyleInit = "";
-let dir = [1, 0, 0], column = 0, preColumn = 0, preYear = 0, preFilter = 0, totalEmission = 0;
+let dir = [1, 0, 0], column = 0, preColumn = 0, hasFilter = true, preYear = 0, preFilter = 0, totalEmission = 0;
 let errorHandle = 0;
+let dataAvailable = [1, 0, 0, 0];
 
 loadData();
 
@@ -57,7 +58,6 @@ function isEmissionNumber(myArr) {
   let emissionNotValid;
   i = 0;
   while (i < myArr.length) {
-
     if (isNaN(myArr[i].emission)) {
       emissionNotValid = 1;
       break;
@@ -81,10 +81,11 @@ function writeHtmlFilterSearch() {
 function writeHtmlFilterButtons() {
   htmlFilter = "";
   htmlFilter += "<button type='button' class='filterbutton active py-2 my-2 g-1' onclick='changeFilterButton(0)'>" + tableHead[0];
+  htmlFilter += "</button>";
   htmlFilter += "<button type='button' class='filterbutton py-2 my-2 g-1' onclick='changeFilterButton(1)'>" + tableHead[1];
   htmlFilter += "</button>";
-
   document.getElementById("myFilterButtons").innerHTML = htmlFilter;
+  filterButtonId = document.getElementById("myFilterButtons").getElementsByTagName("button");
 }
 
 // HTML für Tabelle aus Array erzeugen
@@ -93,7 +94,6 @@ function writeHtmlTable(myArr) {
   // Tabelle anlegen
   htmlTable = "<table class='table table-hover table-bordered table-light' id='javaTable'>";
   htmlTable += "<tr class='table-dark'>";
-
   // Tabellenkopf erzeugen
   for (x = 0; x < tableHead.length - 1; x++) {
     htmlTable += "<th class='px-1 px-md-4 py-2 py-md-1 align-top w-25 tabletext";
@@ -120,59 +120,63 @@ function writeHtmlTable(myArr) {
   htmlTable += "<tr style='display: none'><td class='p-3'><em>No data found.</em></td><td class='p-3'> </td><td class='p-3'> </td><td class='p-3'> </td></tr>";
   // Tabelle schließen
   htmlTable += "</table>";
-  htmlTable += "<p id='measurement'>The value for carbon emission are measured in Gigatons. In 2023 we have tracked " + myArr.length + " companies with a total emission of " + totalEmission + " Gigatons CO2.</p>";
+  htmlTable += "<p id='measurement'>The value for carbon emission are measured in Gigatons. In 2023 we have tracked " + myArr.length + " companies with ";
+  htmlTable += "a total emission of " + totalEmission + " Gigatons CO2.</p>";
+
   // erzeugtes HTML im DOM aktuallisieren
   document.getElementById("jsTableDom").innerHTML = htmlTable;
 };
 
 // Tabelle filter
 function myFilter(column) {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myFilterInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("javaTable");
-  tr = table.getElementsByTagName("tr");
-  tableLenght = myArr.length;
-  for (i = 0; i < tr.length - 1; i++) {
-    td = tr[i].getElementsByTagName("td")[column];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-        tableLenght++;
-      } else {
-        tr[i].style.display = "none";
-        tableLenght--;
+  if (hasFilter) {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myFilterInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("javaTable");
+    tr = table.getElementsByTagName("tr");
+    tableLenght = myArr.length;
+    
+    for (i = 0; i < tr.length - 1; i++) {
+      td = tr[i].getElementsByTagName("td")[column];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+          tableLenght++;
+        } else {
+          tr[i].style.display = "none";
+          tableLenght--;
+        }
       }
     }
-  }
-  if (tableLenght == 0) {
-    tr[myArr.length + 1].style.display = "";
-  }
-  else {
-    tr[myArr.length + 1].style.display = "none";
+    if (tableLenght == 0) {
+      tr[myArr.length + 1].style.display = "";
+    }
+    else {
+      tr[myArr.length + 1].style.display = "none";
+    }
   }
 }
 
 // Filter Buttons umschalten
 function changeFilterButton(filterButton) {
-  let filterButtonId;
-  filterButtonId = document.getElementById("myFilterButtons").getElementsByTagName("button");
+
   if (filterButton == 0) {
     filterButtonId[0].classList.add("active")
     filterButtonId[1].classList.remove("active")
     searchFor = tableHead[0];
     column = 0;
-    writeHtmlFilterSearch();
-    writeHtmlTable(myArr);
   }
-  else {
+  else if (filterButton == 1) {
+
     filterButtonId[1].classList.add("active")
     filterButtonId[0].classList.remove("active")
     searchFor = tableHead[1];
     column = 1;
-    writeHtmlFilterSearch();
+  }
+  writeHtmlFilterSearch();
+  if (hasFilter) {
     writeHtmlTable(myArr);
   }
 }
@@ -182,29 +186,27 @@ function getTotalEmission() {
   for (x = 0; x < myArr.length; x++) {
     totalEmission = totalEmission + Number(myArr[x].emission);
 
-;  }
-totalEmission = totalEmission.toFixed(2);
+    ;
+  }
+  totalEmission = totalEmission.toFixed(2);
   return totalEmission;
 }
 
 // Tabelle sortieren nach Auswahl der Spalte in der Variable Column
 function sortTable(column) {
   var table, rows, switching, i, x, y, shouldSwitch;
-
   // Prüfung ob die aktuell geklickte Spalte mit der zuvor geklickten Spalte gleich ist
   if (preColumn == column) {
     //wenn ja, Sortierrichtung der Spalte invertieren
     dir[column] = !dir[column]
   }
   // Aktive Klasse vom zuvor gekkickten Button entfernen
-  document.getElementById(("sortBtn" + preColumn)).classList.remove("active");
   // Aktive Klasse zum aktuell geklickten Button hinzufügen
+  document.getElementById(("sortBtn" + preColumn)).classList.remove("active");
   document.getElementById(("sortBtn" + column)).classList.add("active");
+
   // aktuelle Spallte speichern.
   preColumn = column;
-  // Referenz auf Tabelle im DOM setzen
-  table = document.getElementById("javaTable");
-  switching = true;
 
   // Icon je nach Sortierrichtung rotieren.
   if (dir[column] == 0) {
@@ -214,48 +216,73 @@ function sortTable(column) {
     document.getElementById(("sortBtn" + column)).style.rotate = "180deg";
   }
 
+  // Referenz auf Tabelle im DOM setzen
+  table = document.getElementById("javaTable");
+  switching = true;
   //Loop der so lange läuft bis keine Zeilen mehr getauscht werden
   while (switching) {
-    //start by saying: no switching is done:
     switching = false;
     rows = table.rows;
     // Loop durch alle Tabellenzeilen, außer der ersten (Header) und der letzten (No Data)
     for (i = 1; i < (rows.length - 2); i++) {
-      //start by saying there should be no switching:
+      //start mit dem Defaultwert kein Tausch
       shouldSwitch = false;
       // Referenz auf die Tabellenzelle der aktuellen Zeile sowie der folgenden Zeile
       x = rows[i].getElementsByTagName("TD")[column];
       y = rows[i + 1].getElementsByTagName("TD")[column];
-      //Spalten Company und Country als String aufsteigend
+      //Spalten Company und Country als String aufsteigend vergleichen und wenn ja dann Tausch marieren
       if (dir[column] == 0 && column < 2 && x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        //if so, mark as a switch and break the loop:
         shouldSwitch = true;
         break;
       }
-      //Spalten Company und Country als String absteigend
+      //Spalten Company und Country als String absteigend vergleichen und wenn ja dann Tausch marieren
       else if (dir[column] == 1 && column < 2 && x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
         shouldSwitch = true;
         break;
       }
-      //Spalte Emissionswert als Zahl aufsteigend
+      //Spalte Emissionswert als Zahl aufsteigend vergleichen und wenn ja dann Tausch marieren
       else if (dir[column] == 0 && column == 2 && Number(x.innerHTML) > Number(y.innerHTML)) {
         shouldSwitch = true;
         break;
       }
-      //Spalte Emissionswert als Zahl absteigend
+      //Spalte Emissionswert als Zahl absteigend vergleichen und wenn ja dann Tausch marieren
       else if (dir[column] == 1 && column == 2 && Number(x.innerHTML) < Number(y.innerHTML)) {
         shouldSwitch = true;
         break;
       }
     }
     if (shouldSwitch) {
-      /*If a switch has been marked, make the switch and mark that a switch has been done:*/
+      //Wenn ein Tausch markiert wurde, Tausch durchführen und markieren, dass ein Tausch durchgeführt wurde
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
     }
   }
 }
 
+// hat Daten für das Jahr
+function hasData(year) {
+  changeYear(year);
+  var table, tr, td, i;
+  table = document.getElementById("javaTable");
+  tr = table.getElementsByTagName("tr");
+  if (dataAvailable[year]) {
+    for (i = 1; i < tr.length - 1; i++) {
+      tr[i].style.display = "";
+    }
+    tr[tr.length - 1].style.display = "none";
+    document.getElementById("measurement").style.display = "";
+    hasFilter = true;
+    myFilter(column);
+  }
+  else {
+    for (i = 1; i < tr.length - 1; i++) {
+      tr[i].style.display = "none";
+    }
+    tr[tr.length - 1].style.display = "";
+    document.getElementById("measurement").style.display = "none";
+    hasFilter = false;
+  }
+}
 function changeYear(year) {
   yearBtn = document.getElementById("hauptnavigation");
   yearBtn = yearBtn.getElementsByTagName("button");
@@ -263,29 +290,4 @@ function changeYear(year) {
   yearBtn[year].classList.add("active")
   preYear = year;
   return year;
-}
-// Tabelle filtern
-function noData(year) {
-
-  changeYear(year);
-  var table, tr, td, i;
-  table = document.getElementById("javaTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 1; i < tr.length - 1; i++) {
-    tr[i].style.display = "none";
-  }
-
-  tr[tr.length - 1].style.display = "";
-  document.getElementById("measurement").style.display = "none";
-}
-function hasData(year) {
-  changeYear(year);
-  var table, tr, td, i;
-  table = document.getElementById("javaTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 1; i < tr.length - 1; i++) {
-    tr[i].style.display = "";
-  }
-  tr[tr.length - 1].style.display = "none";
-  document.getElementById("measurement").style.display = "";
 }
--- language.js
+++ language.js
@@ -1,21 +1,21 @@
 function setLanguage(language) {
 
-    let buttonId;
-    buttonId = document.getElementById("langselect").getElementsByTagName("button")
+    let langButtonId;
+    langButtonId = document.getElementById("langselect").getElementsByTagName("button")
     if (language == 0) {
         if (document.getElementById("hauptnavigation")) {
       document.getElementById("hauptnavigation").style.float = "left";
     }
-      buttonId[0].classList.add("active");
-      buttonId[1].classList.remove("active");
+      langButtonId[0].classList.add("active");
+      langButtonId[1].classList.remove("active");
   
     }
     else {
         if (document.getElementById("hauptnavigation")) {
       document.getElementById("hauptnavigation").style.float = "right";
     }
-      buttonId[1].classList.add("active");
-      buttonId[0].classList.remove("active");
+      langButtonId[1].classList.add("active");
+      langButtonId[0].classList.remove("active");
   
     }
   }
